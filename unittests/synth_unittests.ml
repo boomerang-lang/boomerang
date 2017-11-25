4788b0190086632688a1f91bfe5688bdbb6f6e05
Anders Miltner <Anders.Miltner@gmail.com>
2017-11-25 11:46:55 -0600

    add basic unittesting capabilities

---
+++ unittests/synth_unittests.ml
@@ -0,0 +1,15 @@
+open Stdlib
+open OUnit2
+open Ounit_extensions
+
+let test_abc _ =
+  assert_equal
+    false
+    true
+
+let normalize_from_tree_suite = "Test NormalizedPTST from_tree" >:::
+  [
+    "test_abc" >:: test_abc;
+  ]
+
+let _ = run_test_tt_main normalize_from_tree_suite
